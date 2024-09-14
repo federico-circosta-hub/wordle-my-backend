1b3be65580dd82907cd89aa64f70a526794e91b0
fridericus-hub <fed.circosta@gmail.com>
2024-09-14 23:45:11 +0200

    cors fix

--- routes/routes.py
+++ routes/routes.py
@@ -3,7 +3,7 @@ from services.word_services import get_word, check_word
 from flask_cors import CORS
 
 bp = Blueprint('wordle_routes', __name__)
-CORS(bp)
+CORS(bp, resources={r"/*": {"origins": "http://localhost:3000/"}})
 
 @bp.route("/")
 def init():
@@ -18,5 +18,4 @@ def api_check_word():
         return jsonify({"error": "Missing word_attempt in request body"}), 400
     result = check_word(word_attempt)
     response = jsonify(result)
-    response.headers['Access-Control-Allow-Origin'] = '*'
     return response
