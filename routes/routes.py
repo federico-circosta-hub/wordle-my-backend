6f69194614a9b6a6729c6a48cb78ecea3803bb35
fridericus-hub <fed.circosta@gmail.com>
2024-09-15 00:38:35 +0200

    v

--- routes/routes.py
+++ routes/routes.py
@@ -8,16 +8,19 @@ def init():
     word = get_word()
     return jsonify(word["day"])
 
-@bp.route("/check_word", methods=['POST','OPTIONS'])
+@bp.route("/check_word", methods=['POST', 'OPTIONS'])
 def api_check_word():
     if request.method == 'OPTIONS':
         response = jsonify({"message": "CORS preflight response"})
         response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
         return response, 200
-    get_word()
+
     word_attempt = request.json.get('word_attempt')
     if not word_attempt:
         return jsonify({"error": "Missing word_attempt in request body"}), 400
+
     result = check_word(word_attempt)
     response = jsonify(result)
     response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
