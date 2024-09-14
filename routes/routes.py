eaf1f4bf4110bfb092efc639d488cbd73ca5899f
fridericus-hub <fed.circosta@gmail.com>
2024-09-14 23:59:34 +0200

    c

--- routes/routes.py
+++ routes/routes.py
@@ -3,14 +3,14 @@ from services.word_services import get_word, check_word
 from flask_cors import CORS
 
 bp = Blueprint('wordle_routes', __name__)
-CORS(bp, resources={r"/*": {"origins": "http://localhost:3000/"}})
+CORS(bp, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 @bp.route("/")
 def init():
     word = get_word()
     return jsonify(word["day"])
 
-@bp.route("/check_word", methods=['POST, OPTIONS'])
+@bp.route("/check_word", methods=['POST'])
 def api_check_word():
     get_word()
     word_attempt = request.json.get('word_attempt')
@@ -18,4 +18,5 @@ def api_check_word():
         return jsonify({"error": "Missing word_attempt in request body"}), 400
     result = check_word(word_attempt)
     response = jsonify(result)
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
     return response
