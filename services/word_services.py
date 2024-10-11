16d6f3bd436c38a89588524d633df64704b871ea
fridericus-hub <fed.circosta@gmail.com>
2024-10-11 09:15:50 +0200

    get word new logic implemented

--- services/word_services.py
+++ services/word_services.py
@@ -1,21 +1,27 @@
-import random
-from datetime import datetime, timedelta
+from datetime import datetime
 
-word = {"value": "abaca", "day": (datetime.now() - timedelta(days=1)).date()}
+start_date = datetime(2020, 1, 1)
+total_words = 3480
+
+word = {"value": "mamma", "day": datetime(1970, 1, 1).date()}
 
 def get_word():
     if word["day"] < datetime.now().date():
-        word["value"] = get_random_word()
+        word["value"] = calulate_word_of_the_day()
         word["day"] = datetime.now().date()
     print("get_word called, word: ", word["day"], word["value"] )
     return word
 
-def get_random_word():
-    with open('five_letter_words.txt', 'r') as file:
+def calulate_word_of_the_day():
+    index = map_day_to_number(total_days_since_start(start_date, datetime.now()), total_words)
+    with open('shuffled_five_letter_words.txt', 'r') as file:
         words = file.readlines()
-    return random.choice(words).strip()
+    return words[index].strip()
 
 def check_word(word_attempt):
+    print("word_attempt", word_attempt)
+    if word["day"] < datetime.now().date():
+        get_word()
     word_characters_occorrence_map = {
         char: word["value"].count(char) for char in word["value"]
     }
@@ -31,3 +37,20 @@ def check_word(word_attempt):
         if word_attempt[i] in word_characters_occorrence_map and word_characters_occorrence_map[word_attempt[i]] > 0:
             word_characters_occorrence_map[word_attempt[i]] -= 1
     return result
+
+def is_leap_year(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+def days_in_year_until(date):
+    start_of_year = datetime(date.year, 1, 1)
+    return (date - start_of_year).days + 1
+
+def total_days_since_start(start_date, current_date):
+    total_days = 0
+    for year in range(start_date.year, current_date.year):
+        total_days += 366 if is_leap_year(year) else 365
+    total_days += days_in_year_until(current_date)
+    return total_days
+
+def map_day_to_number(total_days, max_number):
+    return total_days % max_number 
