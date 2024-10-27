dae4543da7ac48082c55c383da163699f6605232
fridericus-hub <fed.circosta@gmail.com>
2024-10-28 00:15:56 +0100

    avoid options method

--- routes/routes.py
+++ routes/routes.py
@@ -11,18 +11,11 @@ bp = Blueprint("wordle-my", __name__)
 
 @bp.route("/")
 def init():
-    print("datetime.now()",datetime.now())
     word = get_word()
     return jsonify(word["day"])
 
-@bp.route("/check_word", methods=("GET","POST", "OPTIONS"))
+@bp.route("/check_word", methods=("GET","POST"))
 def api_check_word():
-    if request.method == 'OPTIONS':
-        response = jsonify({"message": "CORS preflight response"})
-        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-        response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        return response, 200
     word_attempt = request.json.get('word_attempt')
     if not word_attempt:
         return jsonify({"error": "Missing word_attempt in request body"}), 400
